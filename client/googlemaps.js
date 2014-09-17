34971e57ab2b6493e7debecbe1f5bc8c03204a2c
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-17 13:19:37 +0400

    loading google maps asynchronously

--- client/googlemaps.js
+++ client/googlemaps.js
@@ -1,18 +1,20 @@
-var HOST = 'https://maps.googleapis.com/maps/api/js';
+var Promise = require('bluebird');
+
+var API_URL = 'https://maps.googleapis.com/maps/api/js';
 
 module.exports = function(opts) {
+  return new Promise(function(resolve, reject) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    var baseUrl = API_URL + '?v=3.ex&sensor=false&callback=onGoogleMapLoaded&libraries=geometry';
 
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  var baseUrl = HOST + '?v=3.ex&sensor=false&callback=onGoogleMapLoaded&libraries=geometry';
+    if ( opts && opts.apikey ) {
+      script.src = baseUrl+'&key=' + opts.apikey;
+    } else {
+      script.src = baseUrl;
+    }
 
-  if ( opts && opts.apikey ) {
-    script.src = baseUrl+'&key=' + opts.apikey;
-  } else {
-    script.src = baseUrl;
-  }
-
-  document.body.appendChild(script);
-  window.onGoogleMapLoaded = opts && opts.callback ;
-
+    document.body.appendChild(script);
+    window.onGoogleMapLoaded = resolve ;
+  });
 };
--- client/main.js
+++ client/main.js
@@ -2,10 +2,8 @@ var googlemaps = require('./googlemaps');
 
 // loading google maps
 
-googlemaps({
-  callback: function () {
-   console.log('google maps loaded');
-  }
+googlemaps().then(function() {
+  console.log('maps loaded');
 });
 
 
