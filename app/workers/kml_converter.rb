248ce6255d73d46e385644af167e793f9b3faee0
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-17 11:47:12 +0400

    more old staff

--- app/workers/kml_converter.rb
+++ app/workers/kml_converter.rb
@@ -5,7 +5,6 @@ class KMLConverter
   def perform(type, path)
     kml = File.read(path)
     case type
-    when 'Objects::Path::Line' then Objects::Path::Line.from_kml(kml)
     when 'Objects::Line' then Objects::Line.from_kml(kml)
     when 'Objects::Tower' then Objects::Tower.from_kml(kml)
     end
