4f88de81bf47d0a96fc0c0629abb0045032a7dab
Dimitri Kurashvili <dimakura@gmail.com>
2014-12-27 15:54:27 +0400

    index region by name

--- app/models/region.rb
+++ app/models/region.rb
@@ -18,6 +18,8 @@ class Region
   has_and_belongs_to_many :users, class_name: 'Sys::User'
   validates :name, presence: {message: 'ჩაწერეთ სახელი'}
 
+  index({ name: 1 })
+
   def self.get_by_name(name); Region.where(name:name).first || Region.create(name:name) end
   def can_delete?; lines.empty? and  towers.empty? and offices.empty? and substations.empty? end
   def towers_limited; self.towers.paginate(per_page:50) end
