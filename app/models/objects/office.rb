6abc30cc1031fea8c4611260ef042e04195729f3
Dimitri Kurashvili <dimakura@gmail.com>
2015-01-18 17:57:55 +0400

    fix office parsing

--- app/models/objects/office.rb
+++ app/models/objects/office.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
+require 'xml'
+
 class Objects::Office
   include Mongoid::Document
   include Objects::Coordinate
@@ -11,28 +13,25 @@ class Objects::Office
   belongs_to :region
 
   def self.from_kml(xml)
-    parser=XML::Parser.string xml
-    doc=parser.parse ; root=doc.child
-    kmlns="kml:#{KMLNS}"
-    placemarks=doc.child.find '//kml:Placemark',kmlns
+    parser = XML::Parser.string xml
+    doc = parser.parse ; root=doc.child
+    kmlns = "kml:#{KMLNS}"
+    placemarks = doc.child.find '//kml:Placemark',kmlns
     placemarks.each do |placemark|
-      id=placemark.attributes['id']
-      name=placemark.find('./kml:name',kmlns).first.content
+      id = placemark.attributes['id']
+      name = placemark.find('./kml:name',kmlns).first.content
       # description content
-      descr=placemark.find('./kml:description',kmlns).first.content
-      s1='<td>რაიონი</td>'
-      s2='<td>მისამართი</td>'
-      idx1=descr.index(s1)+s1.length
-      idx2=descr.index(s2)+s2.length
-      regname=descr[idx1..-1].match(/<td>([^<])*<\/td>/)[0][4..-6].strip
-      address=descr[idx2..-1].match(/<td>([^<])*<\/td>/)[0][4..-6].strip
-      region=Region.get_by_name(regname)
+      descr = placemark.find('./kml:description',kmlns).first.content
+      # regname = Objects::Kml.get_property(descr, 'რაიონი') ----> ???
+      regname = Objects::Kml.get_property(descr, 'რეგიონი')
+      address = Objects::Kml.get_property(descr, 'მისამართი')
       # end of description section
-      coord=placemark.find('./kml:Point/kml:coordinates',kmlns).first.content
-      obj=Objects::Office.where(kmlid:id).first || Objects::Office.create(kmlid:id)
-      obj.name=name
-      obj.region=region
-      obj.address=address
+      region = Region.get_by_name(regname)
+      coord = placemark.find('./kml:Point/kml:coordinates',kmlns).first.content
+      obj = Objects::Office.where(kmlid:id).first || Objects::Office.create(kmlid:id)
+      obj.name = name
+      obj.region = region
+      obj.address = address
       obj.set_coordinate(coord)
       obj.save
     end
