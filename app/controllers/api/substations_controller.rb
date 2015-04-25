62316ca4ecea65d18020d2b3165c3cbb27383b4c
Nick Shvelidze <captain@pirrate.me>
2015-04-25 21:36:16 +0400

    within_box

--- app/controllers/api/substations_controller.rb
+++ app/controllers/api/substations_controller.rb
@@ -1,7 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Api::SubstationsController < ApiController
   def index
-    substations = Objects::Substation.all
+    if params["bounds"] then
+      bounds = params["bounds"].split(',')
+      substations = Objects::Substation.within_box([bounds[0], bounds[1]], [bounds[2], bounds[3]])
+    else
+      substations = Objects::Substation.all
+    end
     render json: (substations.map do |substation|
       { id: substation.id.to_s, lat: substation.lat, lng: substation.lng, name: substation.name }
     end)
