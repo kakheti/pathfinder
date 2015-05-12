65f7db62e931f80d7fe883b8c3958f61b4b7bec8
Nick Shvelidze <captain@pirrate.me>
2015-05-12 17:57:45 +0400

    Search by multiple types

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -5,27 +5,33 @@ class Api::SearchController < ApiController
       %w(region_id).include?(key) && value.length > 0
     }
 
-    type = params["type"].to_sym unless params["type"].nil?
+    types = []
+    types = params["type"] unless params["type"].nil?
 
     object_types = {
-      line: Objects::Line,
-      pole: Objects::Pole,
-      substation: Objects::Substation,
-      tower: Objects::Tower,
-      tp: Objects::Tp,
-      fider: Objects::Fider,
-      office: Objects::Office
+      "line" => Objects::Line,
+      "pole" => Objects::Pole,
+      "substation" => Objects::Substation,
+      "tower" => Objects::Tower,
+      "tp" => Objects::Tp,
+      "fider" => Objects::Fider,
+      "office" => Objects::Office
     }
 
-    if !object_types[type].nil? then
-      objects = object_types[type].all(filters)
-      objects = objects.full_text_search(params["name"]) if params["name"] && params["name"].length > 0
-      
-      render json: (objects.map do |object|
-      { id: object.id.to_s, lat: object.lat, lng: object.lng, name: object.name, type: params['type'] }
-    end)
-    else
-      render json: []
+    all_objects = [];
+
+    types.each do |type|
+      if !object_types[type].nil? then
+        objects = object_types[type].all(filters)
+        objects = objects.full_text_search(params["name"]) if params["name"] && params["name"].length > 0
+        all_objects.concat objects
+      end
     end
+
+    render json: (all_objects.map do |object|
+      type = object_types.invert[object.class]
+
+      { id: object.id.to_s, lat: object.lat, lng: object.lng, name: object.name, type: type }
+    end)
   end
 end
