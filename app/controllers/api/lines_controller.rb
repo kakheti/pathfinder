78c6f52cffb701a69b4aed4720f40c9a2beca025
Nick Shvelidze <captain@pirrate.me>
2015-05-30 21:06:13 +0400

    Fiders n lines

--- app/controllers/api/lines_controller.rb
+++ app/controllers/api/lines_controller.rb
@@ -1,13 +1,15 @@
 # -*- encoding : utf-8 -*-
 class Api::LinesController < ApiController
   def index
-    puts params
-    if params["bounds"] then
-      lines = Objects::Line.where(within_bounds(params["bounds"]))
-      lines += Objects::Fider.where(within_bounds(params["bounds"])).map{|x| x.lines}.flatten if params["fiders"]
-    else
-      lines = Objects::Line.all
-      lines += Objects::Fider.all.map{|x| x.lines}.flatten if params["fiders"]
+    params["type"] = ["line"]
+
+    lines = Api::SearchController.search(params)
+
+    if params["fiders"] then
+      params["type"] = ["fider"]
+      fiders = Api::SearchController.search(params)
+      fiders = fiders.map{|x| x.lines}.flatten
+      lines += fiders
     end
 
     render json: {
