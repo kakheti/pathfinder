7655aaec7f1ba3c8470018bbefbaaf3dc01832c5
Dimitri Kurashvili <dimakura@gmail.com>
2016-01-02 17:21:52 +0400

    PolesUploadWorker

---
+++ app/workers/poles_upload_worker.rb
@@ -0,0 +1,21 @@
+class PolesUploadWorker
+  include Sidekiq::Worker
+
+
+  def perform(file)
+    Zip::File.open file do |zip_file|
+      zip_file.each do |entry|
+        upload_kml(entry) if 'kml'==entry.name[-3..-1]
+      end
+    end
+  end
+
+private
+
+  def upload_kml(file)
+    Objects::Pole.delete_all
+    kml = file.get_input_stream.read
+    Objects::Pole.from_kml(kml)
+  end
+
+end
