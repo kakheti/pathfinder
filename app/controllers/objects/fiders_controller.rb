7b109011bd060f1ff00410c744cf9ee017541f2a
Dimitri Kurashvili <dimakura@gmail.com>
2016-01-02 17:16:45 +0400

    FidersUploadWorker

--- app/controllers/objects/fiders_controller.rb
+++ app/controllers/objects/fiders_controller.rb
@@ -69,17 +69,7 @@ class Objects::FidersController < ApplicationController
   private
 
   def upload_kmz(file)
-    Zip::File.open file do |zip_file|
-      zip_file.each do |entry|
-        upload_kml(entry) if 'kml'==entry.name[-3..-1]
-      end
-    end
-  end
-
-  def upload_kml(file)
-    Objects::Fider.delete_all
-    kml = file.get_input_stream.read
-    Objects::Fider.from_kml(kml)
+    FidersUploadWorker.perform_async(file.path)    
   end
 
   def upload_xlsx(file)
