c6dce14398e617a2a4f4240014938ca1a4ad4164
Nick Shvelidze <captain@pirrate.me>
2016-01-04 01:39:50 +0400

    Refactor to_square to include upper bounds

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -13,10 +13,22 @@ class Api::SearchController < ApiController
     'pole04' => Objects::Pole04,
   }
 
-  def self.to_square(lat, lng)
-    x = (lat.to_f * 100).round
-    y = (lng.to_f * 100).round
-    "lat:#{x}:lng:#{y}"
+  def self.to_square(bounds)
+    bounds = bounds.split(',')
+    lat1 = bounds[0]
+    lng1 = bounds[1]
+    lat2 = bounds[2]
+    lng2 = bounds[3]
+    x1 = (lat1.to_f * 100).floor
+    y1 = (lng1.to_f * 100).floor
+    x2 = (lat2.to_f * 100).ceil
+    y2 = (lng2.to_f * 100).ceil
+    key = "lat1:#{x1}:lng1:#{y1}:lat2:#{x2}:lng2:#{y2}"
+    bounds[0] = x1 / 100.0
+    bounds[1] = y1 / 100.0
+    bounds[2] = x2 / 100.0
+    bounds[3] = y2 / 100.0
+    [key, bounds]
   end
 
   def self.to_jsonable(objects)
@@ -57,12 +69,12 @@ class Api::SearchController < ApiController
     types.each do |type|
       key = nil
       if params['bounds'] && !params['name']
-        bounds_split = params['bounds'].split(',')
-        square = self.to_square(bounds_split[0], bounds_split[1])
+        square = self.to_square(params['bounds'])
+        bounds = square[1]
         if geojson
-          key = "geodata:#{square}:#{type}:geojson"
+          key = "geodata:#{square[0]}:#{type}:geojson"
         else
-          key = "geodata:#{square}:#{type}"
+          key = "geodata:#{square[0]}:#{type}"
         end
         cached = $redis.get(key)
         data = JSON.parse(cached) rescue nil
@@ -72,19 +84,25 @@ class Api::SearchController < ApiController
               obj['region'] && obj['region']['id'] == params['region_id']
             }
           end
-          all_objects.concat(data)
+
+          if data.kind_of? Array
+            all_objects.concat(data)
+          else
+            all_objects.push(data)
+          end
+
           next
         end
       end
 
       objects = @@object_types[type].all
       if type == 'fider-line' && params['bounds']
-        objects = objects.where({lines: {'$elemMatch' => {points: {'$elemMatch' => within_bounds(params['bounds'])}}}})
+        objects = objects.where({lines: {'$elemMatch' => {points: {'$elemMatch' => within_bounds(bounds)}}}})
         objects = objects.map { |obj| obj.lines }.flatten
       elsif type == 'fider04' && params['bounds']
-        objects = objects.where({points: {'$elemMatch' => within_bounds(params['bounds'])}})
+        objects = objects.where({points: {'$elemMatch' => within_bounds(bounds)}})
       elsif type != 'line' && params['bounds']
-        objects = objects.where(within_bounds(params['bounds']))
+        objects = objects.where(within_bounds(bounds))
       end
 
       unless key.nil?
