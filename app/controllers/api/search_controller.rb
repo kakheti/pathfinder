aaa36e283d83ff0bbb75e60b219a70ae65c7ffce
Nick Shvelidze <captain@pirrate.me>
2016-01-03 20:19:50 +0400

    Refactor SearchController

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -16,7 +16,7 @@ class Api::SearchController < ApiController
   def self.to_square(lat, lng)
     x = (lat.to_f * 100).round
     y = (lng.to_f * 100).round
-    "#{x}#{y}"
+    "lat:#{x}:lng:#{y}"
   end
 
   def self.to_jsonable(objects)
@@ -24,11 +24,11 @@ class Api::SearchController < ApiController
       unless object.is_a? Hash
         type = @@object_types.invert[object.class]
 
-        region = { name: object.region_name, id: object.region_id.to_s } if object.respond_to?(:region) && !object.region_id.nil?
-        substation = { name: object.substation_name, id: object.substation_id.to_s } if object.respond_to?(:substation) && !object.substation_id.nil?
-        line = { name: object.linename, id: object.line_id.to_s } if object.respond_to?(:line) && !object.line_id.nil?
-        fider = { name: object.fider_name, id: object.fider_id.to_s } if object.respond_to?(:fider_id) && !object.fider_id.nil?
-        tp = { name: object.tp_name, id: object.tp_id.to_s } if object.respond_to?(:tp) && !object.tp_id.nil?
+        region = {name: object.region_name, id: object.region_id.to_s} if object.respond_to?(:region) && !object.region_id.nil?
+        substation = {name: object.substation_name, id: object.substation_id.to_s} if object.respond_to?(:substation) && !object.substation_id.nil?
+        line = {name: object.linename, id: object.line_id.to_s} if object.respond_to?(:line) && !object.line_id.nil?
+        fider = {name: object.fider_name, id: object.fider_id.to_s} if object.respond_to?(:fider_id) && !object.fider_id.nil?
+        tp = {name: object.tp_name, id: object.tp_id.to_s} if object.respond_to?(:tp) && !object.tp_id.nil?
 
         object.name = "\##{object.number} #{object.name}" if type == 'substation'
 
@@ -42,7 +42,7 @@ class Api::SearchController < ApiController
           line: line,
           fider: fider,
           tp: tp,
-          type: type }
+          type: type}
       else
         object
       end
@@ -54,54 +54,52 @@ class Api::SearchController < ApiController
       %w(region_id).include?(key) && value.length > 0
     }
 
-    types = @@object_types.keys
-    types = params['type'] unless params['type'].nil?
+    types = ( params['type'] || @@object_types.keys ) & @@object_types.keys
 
     all_objects = []
 
     types.each do |type|
-      unless @@object_types[type].nil?
-        cache_key = nil
-        if params['bounds'] && !params['name']
-          bounds_split = params['bounds'].split(',')
-          square = self.to_square(bounds_split[0], bounds_split[1])
-          if geojson
-            cache_key = "#{square}#{type}geojson"
-          else
-            cache_key = "#{square}#{type}"
-          end
-          cached = $redis.get(cache_key)
-          unless cached.nil?
-            begin
-              all_objects.concat JSON.parse(cached)
-              break
-            rescue
-              $redis.del(square+type)
-            end
-          end
+      cache_key = nil
+      if params['bounds'] && !params['name']
+        bounds_split = params['bounds'].split(',')
+        square = self.to_square(bounds_split[0], bounds_split[1])
+        if geojson
+          cache_key = "#{square}:#{type}:geojson"
+        else
+          cache_key = "#{square}:#{type}"
         end
-
-        objects = @@object_types[type].all(filters)
-        if type == 'fider-line' && params['bounds']
-          objects = objects.where({lines: {'$elemMatch' => {points: {'$elemMatch' => within_bounds(params['bounds'])}}}})
-          objects = objects.map { |obj| obj.lines }.flatten
-        elsif type == 'fider04' && params['bounds']
-          objects = objects.where({points: {'$elemMatch' => within_bounds(params['bounds'])}})
-        elsif type != 'line' && params['bounds']
-          objects = objects.where(within_bounds(params['bounds']))
-        end
-        objects = objects.full_text_search(params['name'], match: :all).limit(5) if params['name'] && params['name'].length > 0
-        all_objects.concat objects
-
-        unless cache_key.nil?
-          if geojson
-            $redis.set(cache_key, Api::LinesController.to_geojson(objects).to_json)
-          else
-            $redis.set(cache_key, Api::SearchController.to_jsonable(objects).to_json)
+        cached = $redis.get(cache_key)
+        unless cached.nil?
+          begin
+            all_objects.concat JSON.parse(cached)
+            break
+          rescue
+            $redis.del(square+type)
           end
-          $redis.expire(cache_key, 1.hour)
         end
       end
+
+      objects = @@object_types[type].all(filters)
+      if type == 'fider-line' && params['bounds']
+        objects = objects.where({lines: {'$elemMatch' => {points: {'$elemMatch' => within_bounds(params['bounds'])}}}})
+        objects = objects.map { |obj| obj.lines }.flatten
+      elsif type == 'fider04' && params['bounds']
+        objects = objects.where({points: {'$elemMatch' => within_bounds(params['bounds'])}})
+      elsif type != 'line' && params['bounds']
+        objects = objects.where(within_bounds(params['bounds']))
+      end
+      objects = objects.full_text_search(params['name'], match: :all).limit(5) if params['name'] && params['name'].length > 0
+      all_objects.concat objects
+
+      unless cache_key.nil?
+        if geojson
+          $redis.set(cache_key, Api::LinesController.to_geojson(objects).to_json)
+        else
+          $redis.set(cache_key, Api::SearchController.to_jsonable(objects).to_json)
+        end
+        $redis.expire(cache_key, 1.hour)
+      end
+
     end
 
     return all_objects
@@ -112,4 +110,5 @@ class Api::SearchController < ApiController
 
     render json: (Api::SearchController.to_jsonable objects)
   end
+
 end
