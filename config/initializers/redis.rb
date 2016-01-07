b4f80acdfc83b8b375fb2ca94fd1d634c5ea3be3
Dimitri Kurashvili <dimakura@gmail.com>
2016-01-07 14:06:40 +0400

    using password with Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 # -*- encoding : utf-8 -*-
 
-$redis = Redis.new(:url => 'redis://localhost/15')
+$redis = Redis.new(url: 'redis://localhost/15', password: ENV['REDIS_SECRET'])
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost/2' }
+  config.redis = { url: 'redis://localhost/2', password: ENV['REDIS_SECRET'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost/2' }
+  config.redis = { url: 'redis://localhost/2', password: ENV['REDIS_SECRET'] }
 end
