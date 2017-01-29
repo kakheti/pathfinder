ef8d7f349599eeb076ffb571519ff92a545ea501
Nick Shvelidze <captain@pirrate.me>
2017-01-30 00:28:49 +0400

    Fix incompatibility

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -27,5 +27,6 @@ module Mongoid
   end
 end
 
-Rails.application.config.active_record.belongs_to_required_by_default = false
-Mongoid::Config.belongs_to_required_by_default = false
+# Required for Rails 5
+#Rails.application.config.active_record.belongs_to_required_by_default = false
+#Mongoid::Config.belongs_to_required_by_default = false
